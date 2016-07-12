4eb0a44e4227f4df9070395ff26af8998aa8bc55
Marcos Ortiz <marcos.ortiz@icloud.com>
2016-07-12 17:07:37 -0500

    Still fixing forker logs.

--- lib/easy_daemons/helpers/forker.rb
+++ lib/easy_daemons/helpers/forker.rb
@@ -10,14 +10,14 @@ module EasyDaemons
             def fork_worker(worker)
                 fork do
                     logger = worker.instance_variable_get(:@logger)
-                    logger.info "Starting #{worker.name} worker in the background ..."
+                    logger.info "Starting #{worker.name} worker in the background ..." rescue nil
                     Process.setproctitle(worker.name)
                     Process.setsid
                     pid = curr_pid
-                    logger.info "#{worker.name} worker successfully started in the background (pid=#{pid})."
-                    logger.info "Refreshing #{worker.pid_file_path} file with #{pid}."
+                    logger.info "#{worker.name} worker successfully started in the background (pid=#{pid})." rescue nil
+                    logger.info "Refreshing #{worker.pid_file_path} file with #{pid}." rescue nil
                     refresh_pid_file(worker.pid_file_path, pid)
-                    logger.info "#{worker.pid_file_path} successfully created with #{pid}."
+                    logger.info "#{worker.pid_file_path} successfully created with #{pid}." rescue nil
                     worker.run
                 end
             end
